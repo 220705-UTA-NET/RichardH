baf3539e08dd7cbea866c4a8efcd1260d622392b
Richard Hawkins <Richard.Hawkins@revature.com>
2022-07-06 15:24:28 -0400

    add MyFirstDotnet

---
+++ MyFirstDotnet/MyFirstDotnet/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// This is a comment character/symbol
+
+// See https://aka.ms/new-console-template for more information
+
+
+
+// Console.WriteLine is from the System library, and allows us access to write a line of text to the console.
+// (___) parameters. These say we are sending Console.WriteLine whatever is inside the ().
+// "" denote a string litteral. 
+// ; Endline character.
+
+Console.WriteLine("Hello, World!");
